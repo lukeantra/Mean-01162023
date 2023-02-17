ea3ee20198418cc5677a79099afa879ad0c1c21b
lukeyzhong <lukeyzhong@gmail.com>
2023-02-17 11:22:20 -0600

    update

---
+++ constructor-ngoninit/src/app/child/child.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+  formattedValue = '';
+  @Input() valueFromParent = 0;
+
+  constructor() {
+    console.log('Called Constructor method');
+    console.log('constructor' + this.valueFromParent);
+    if(this.valueFromParent < 50){
+      this.formattedValue = 'less than 50';
+    } else {
+      this.formattedValue = 'greater than or equal to 50';
+    }
+   }
+
+  ngOnInit(): void {
+    console.log('Called ngOnInit method');
+    console.log('init' + this.valueFromParent)
+    // if(this.valueFromParent < 50){
+    //   this.formattedValue = 'less than 50';
+    // } else {
+    //   this.formattedValue = 'greater than or equal to 50';
+    // }
+
+  }
+
+}

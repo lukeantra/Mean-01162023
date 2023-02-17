ff9ec837df0d81c716353918cd01b9499ea84cbf
lukeyzhong <lukeyzhong@gmail.com>
2023-02-17 15:03:11 -0600

    update

--- constructor-ngoninit/src/app/child/child.component.ts
+++ constructor-ngoninit/src/app/child/child.component.ts
@@ -13,21 +13,21 @@ export class ChildComponent implements OnInit, OnDestroy {
   constructor() {
     console.log('Called Constructor method');
     console.log('constructor' + this.valueFromParent);
-    if(this.valueFromParent < 50){
-      this.formattedValue = 'less than 50';
-    } else {
-      this.formattedValue = 'greater than or equal to 50';
-    }
-   }
-
-  ngOnInit(): void {
-    console.log('Called ngOnInit method');
-    console.log('init' + this.valueFromParent)
     // if(this.valueFromParent < 50){
     //   this.formattedValue = 'less than 50';
     // } else {
     //   this.formattedValue = 'greater than or equal to 50';
     // }
+   }
+
+  ngOnInit(): void {
+    console.log('Called ngOnInit method');
+    console.log('init' + this.valueFromParent);
+    if(this.valueFromParent < 50){
+      this.formattedValue = 'less than 50';
+    } else {
+      this.formattedValue = 'greater than or equal to 50';
+    }
     
 
   }

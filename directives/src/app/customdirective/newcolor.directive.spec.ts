6a463d3613e0213389d22eec082a70fdc60cf73b
lukeyzhong <lukeyzhong@gmail.com>
2024-06-24 08:57:59 -0500

    update

--- directives/src/app/customdirective/newcolor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NewcolorDirective } from './newcolor.directive';
-
-describe('NewcolorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NewcolorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
